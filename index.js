45a9521fa674fa9b3cb6802c249f66961740d575
SCPassion <141584825+SCPassion@users.noreply.github.com>
2024-11-01 20:22:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertBtn = document.getElementById("convert-btn")
+const inputField = document.getElementById("input-field")
+const lengthEl = document.getElementById("length")
+const volumnEl = document.getElementById("volumn")
+const massEl = document.getElementById("mass")
+
+const lengthDescription = `<p class="param-description">Length (Meter/Feet)</p>`
+const volumnDescription = `<p class="param-description">Volume (Liters/Gallons)</p>`
+const massDescription = `p class="param-description">Mass (Kilograms/Pounds)</p>`
+
+let storeData = {
+    feet: 0,
+    meter: 0,
+    gallon: 0,
+    liter: 0,
+    pound: 0,
+    kilogram: 0
+}
+
+convertBtn.addEventListener("click", function() {
+    let inputData = Number(inputField.value)
+
+    storeData.feet = (inputData * 3.281).toFixed(2)
+    storeData.meter = (inputData / 3.281).toFixed(2)
+
+    storeData.gallon = (inputData * 0.264).toFixed(2)
+    storeData.liter = (inputData / 0.264).toFixed(2)
+
+    storeData.pound = (inputData * 2.204).toFixed(2)
+    storeData.kilogram = (inputData / 2.204).toFixed(2)
+
+    render(inputData, storeData)
+})
+
+function render (input, data) {
+    lengthEl.innerHTML = lengthDescription + `<p class="calculate-result">${input} meters = ${data.feet} feet | ${input} feet = ${data.meter} meters</p>`
+    volumnEl.innerHTML = lengthDescription + `<p class="calculate-result">${input} liters = ${data.gallon} gallons | ${input} gallons = ${data.liter} liters</p>`
+    massEl.innerHTML = lengthDescription + `<p class="calculate-result">${input} kilos = ${data.pound} pounds | ${input} pounds = ${data.kilogram} kilos</p>`
+}
